81cd2aa6cf7c59cf6b72276275aacbffcf198861
ncnghia <32086455+ncnghia@users.noreply.github.com>
2017-09-21 11:11:54 +0700

    Update MessageTest.java
    123

--- my-app/src/test/java/com/mycompany/app/MessageTest.java
+++ my-app/src/test/java/com/mycompany/app/MessageTest.java
@@ -1,1 +1,14 @@
 package app;
+
+import static org.hamcrest.CoreMatchers.contrainsString;
+import static org.junit.Assert.*
+
+import org.junit.Test;
+
+public class MessageTest {
+  
+  private Message message = new Message();
+  
+  @Test
+  
+}
